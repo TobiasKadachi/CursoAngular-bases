9b352e70c10d9de638669ac7a738a764f6c0c272
Victor Martin Arquelladas <vmn@stiga.es>
2024-09-05 15:03:15 +0200

    ngInputs, ngClasses...

--- src/app/dbz/components/form/form.component.ts
+++ src/app/dbz/components/form/form.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
 
 @Component({
   selector: 'dbz-add-character',
   templateUrl: './form.component.html',
   styleUrl: './form.component.css'
 })
+
+@Output()
 export class FormComponent {
 
+  onNewCharacter : EventEmitter<Character> = new EventEmitter();
+
+
+  public character: Character = {
+    name: '',
+    power: 0
+  };
+
+  emitCharacter() : void{
+    console.log(this.character);
+
+    this.character.name = '';
+    this.character.power = 0;
+  }
 }
--- src/app/dbz/dbz.module.ts
+++ src/app/dbz/dbz.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { MainPageComponent } from './pages/main-page.component';
 import { ListComponent } from './components/list/list.component';
 import { FormComponent } from './components/form/form.component';
+
 
 
 
@@ -18,6 +20,7 @@ import { FormComponent } from './components/form/form.component';
   ],
   imports: [
     CommonModule,
+    FormsModule
   ],
 })
 
